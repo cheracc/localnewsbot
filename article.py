367e1ed172fd779be98db9c4b76f04da355eb0f5
cheracc <shawn.stipe+github@gmail.com>
2026-02-03 19:21:01 +0000

    Add web scraping and filtering functionality for news articles
    - Implemented WebNewsSource class for scraping articles from websites using the newspaper library.
    - Added HTML source parsing capabilities to fetch and extract article details.
    - Created NewsFilter class to filter articles based on predefined criteria including headline, body, and URL filters.
    - Introduced RSS_Source class for parsing RSS feeds and extracting articles.
    - Enhanced logging to track fetched articles and filtering results.

---
+++ article.py
@@ -0,0 +1,8 @@
+class PostableArticle:
+    def __init__(self, source_name, headline, description, link, created_at):
+        self.source_name = source_name
+        self.headline = headline
+        self.description = description
+        self.link = link
+        self.created_at = created_at
+
