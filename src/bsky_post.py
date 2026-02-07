0c50e0b09b215a3050f6a2a1986d7914bccff762
cheracc <shawn.stipe+github@gmail.com>
2026-02-07 16:02:58 +0000

    strip newlines from generated text

--- src/bsky_post.py
+++ src/bsky_post.py
@@ -39,7 +39,7 @@ class BskyPost:
     def post_to_bluesky(self) -> None: 
         self.config.logger.info(f"Posting article: {self.headline}")
         start_time = time.time()
-        self.post_text = self.get_post_text()
+        self.post_text = self.get_post_text().rstrip()
         self.config.logger.debug(f"  Generated post text: {self.post_text}")
         self.post_text, tag_str = self.add_tags_to_post()
         self.config.logger.debug(f"  Keyword matched tags: {tag_str}")
