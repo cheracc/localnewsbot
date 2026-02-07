fb6efae80ec5f9983e3e77ebf47a880dcb9f94b1
cheracc <shawn.stipe+github@gmail.com>
2026-02-07 15:39:08 +0000

    more logging

--- src/tags.py
+++ src/tags.py
@@ -16,12 +16,12 @@ def _assign_tags_from_keywords(article: BskyPost, tags_config: Dict[str, list[st
 
     return assigned_tags
 
-def add_tags_to_post(article: BskyPost, tags_config: Dict[str, list[str]]) -> str:
+def add_tags_to_post(article: BskyPost, tags_config: Dict[str, list[str]]) -> tuple[str, str]:
     tags = _assign_tags_from_keywords(article, tags_config)
     tags.append(article.tag)  # Include the tag of the source
     post_text = article.get_post_text()
     if not tags:
-        return post_text
+        return post_text, ""
     tag_str = " ".join(f"#{tag} " for tag in tags)
 
     if len(post_text) + len(tag_str) > 300:
@@ -29,4 +29,4 @@ def add_tags_to_post(article: BskyPost, tags_config: Dict[str, list[str]]) -> st
         allowed_post_length = 300 - len(tag_str) - 4  # -1 for a newline, -3 for ...
         post_text = post_text[:allowed_post_length].rstrip() + "…"
 
-    return f"{post_text.rstrip()}\n{tag_str}"
+    return f"{post_text.rstrip()}\n{tag_str}", tag_str
