61ab2ea6c2451a7d6bb39d06c4ca719e21e70826
cheracc <shawn.stipe+github@gmail.com>
2026-02-08 11:51:08 -0500

    parse args

--- src/commands.py
+++ src/commands.py
@@ -24,8 +24,8 @@ class CommandHandler:
         split = shlex.split(text)
         if not split:
             return None
+        args = split[1:]
         command = split[0].lower()
-        args = " ".join(split[1:]).strip()
         if not command:
             self.config.logger.debug("No commands found")
             return None
@@ -41,12 +41,12 @@ class CommandHandler:
 
 
 class BotCommand:
-    def __init__(self, config: Config, cmd_str: str, cmd_func: Callable[[Config, str], CommandResponse]):
+    def __init__(self, config: Config, cmd_str: str, cmd_func: Callable[[Config, list[str]], CommandResponse]):
         self.config = config
         self.command_string = cmd_str
         self.command_func = cmd_func
 
-    def execute_command(self, config: Config, args: str) -> CommandResponse:
+    def execute_command(self, config: Config, args: list[str]) -> CommandResponse:
         return self.command_func(config, args)
 
 @dataclass
@@ -55,7 +55,7 @@ class CommandResponse:
     response: str
 
 # Basic Commands:
-def help_command(config: Config, word: str) -> CommandResponse:
+def help_command(config: Config, args: list[str]) -> CommandResponse:
     cmds = config.get_bsky_account().get_chat_handler().command_handler.commands
     if not cmds or len(cmds) == 0:
         return CommandResponse(False, "I have no registered commands. This seems to be a bug.")
@@ -63,28 +63,30 @@ def help_command(config: Config, word: str) -> CommandResponse:
     for cmd in cmds:
         cmd_str = f" {cmd.command_string}\n"
         reply = reply + cmd_str
+    reply = reply + "\n\nType a command by itself for syntax hints."
     return CommandResponse(True, reply.rstrip())
 
 # Example message: add_bad_word istanbul
-def add_bad_word_to_filter(config: Config, words: str) -> CommandResponse:
-    if not words:
-        return CommandResponse(False, "Syntax: add_bad_word word1 word2 ...")
-    split = shlex.split(words)
-    for s in split:
+def add_bad_word_to_filter(config: Config, args: list[str]) -> CommandResponse:
+    if not args or len(args) == 0:
+        return CommandResponse(False, 'Syntax: add_bad_word word1 "word two" ...')
+    for s in args:
         config.add_bad_word(s)
-    return CommandResponse(True, f"Added {len(split)} bad words")
+    return CommandResponse(True, f"Added {len(args)} bad words")
 
-def add_good_word_to_filter(config: Config, words: str) -> CommandResponse:
-    if not words:
-        return CommandResponse(False, "Syntax: add_good_word word1 word2 ...")
-    split = shlex.split(words)
-    for s in split:
+def add_good_word_to_filter(config: Config, args: list[str]) -> CommandResponse:
+    if not args:
+        return CommandResponse(False, 'Syntax: add_good_word word1 "word two" ...')
+    for s in args:
         config.add_good_word(s)
-    return CommandResponse(True, f"Added {len(split)} good words")
+    return CommandResponse(True, f"Added {len(args)} good words")
 
-def list_keywords_for_tag(config: Config, tag: str) -> CommandResponse:
-    keyword_string = config.get_tag_keywords(tag)
+def list_keywords_for_tag(config: Config, args: list[str]) -> CommandResponse:
+    if not args or len(args) != 1:
+        return CommandResponse(False, 'Syntax: list_tag_keywords tagNameWithoutHashtag')
+
+    keyword_string = config.get_tag_keywords(args[0])
     if not keyword_string:
-        return CommandResponse(False, f"I do not have any keywords for #{tag}")
+        return CommandResponse(False, f"I do not have any keywords for #{args[0]}")
     return CommandResponse(True, keyword_string)
 
